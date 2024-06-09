e4259430c4214d9836f795bbcb30bafcaf165963
EXG1O <borodenko22448811@gmail.com>
2024-06-10 01:41:07 +0300

    Improve perf of MonacoEditor component

---
+++ src/components/MonacoEditor/index.tsx
@@ -0,0 +1,115 @@
+import React, { ReactElement, memo, useRef, useMemo, useCallback } from 'react';
+import classNames from 'classnames';
+import monaco from 'monaco-editor';
+
+import('./index.scss');
+
+import Editor, { EditorProps, OnMount, OnChange } from '@monaco-editor/react';
+
+import EditorLoading from './components/EditorLoading';
+
+export interface Editor extends monaco.editor.IStandaloneCodeEditor {
+	updateLayout: (shouldResetWidth?: boolean) => void;
+}
+
+export interface MonacoEditorProps extends Omit<EditorProps, 'loading' | 'onChange'> {
+	size?: 'sm' | 'lg';
+	disablePadding?: boolean;
+	onChange?: (editor: Editor, value: string) => void;
+}
+
+const lineHeights: Record<string, number> = { sm: 19, lg: 22 };
+const fontSizes: Record<string, number> = { sm: 14, lg: 18 };
+const roundedValues: Record<string, number> = { sm: 1, lg: 3 };
+
+const baseOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
+	minimap: { enabled: false },
+	renderLineHighlight: 'none',
+	lineNumbersMinChars: 3,
+	overviewRulerLanes: 0,
+	scrollBeyondLastLine: false,
+	scrollbar: { vertical: 'hidden' },
+	inlayHints: { enabled: 'off' },
+	contextmenu: false,
+};
+
+function MonacoEditor({
+	size,
+	disablePadding,
+	options,
+	className,
+	onChange,
+	onMount,
+	...props
+}: MonacoEditorProps): ReactElement<MonacoEditorProps> {
+	const editor = useRef<Editor | undefined>(undefined);
+
+	const lineHeight = useMemo<number>(() => (size ? lineHeights[size] : 22), [size]);
+	const fontSize = useMemo<number>(() => (size ? fontSizes[size] : 16), [size]);
+	const roundedValue = useMemo<number>(
+		() => (size ? roundedValues[size] : 2),
+		[size],
+	);
+
+	const baseClassName = useMemo<string>(
+		() => `border rounded-${roundedValue}`,
+		[roundedValue],
+	);
+
+	const updateLayout = useCallback((resetWidth?: boolean) => {
+		if (!editor.current) return;
+
+		const editorModel: monaco.editor.ITextModel | null = editor.current.getModel();
+
+		if (editorModel === null) return;
+
+		editor.current.layout({
+			width: resetWidth
+				? 0
+				: editor.current.getContainerDomNode().querySelector('.monaco-editor')!
+						.clientWidth,
+			height: editorModel.getLineCount() * lineHeight,
+		});
+	}, []);
+
+	const handleChange = useCallback<OnChange>(
+		(value) => {
+			if (!editor.current || value === undefined) return;
+
+			updateLayout();
+			onChange?.(editor.current, value);
+		},
+		[onChange],
+	);
+
+	const handleMount = useCallback<OnMount>(
+		(baseEditor, monaco) => {
+			editor.current = Object.assign(baseEditor, { updateLayout });
+
+			updateLayout();
+			onMount?.(editor.current, monaco);
+		},
+		[onMount],
+	);
+
+	return (
+		<Editor
+			{...props}
+			loading={<EditorLoading className={baseClassName} />}
+			options={useMemo(
+				() => ({ ...baseOptions, fontSize, ...options }),
+				[options],
+			)}
+			className={classNames(
+				baseClassName,
+				'monaco-editor-wrapper overflow-hidden',
+				{ 'p-show': !disablePadding },
+				className,
+			)}
+			onChange={handleChange}
+			onMount={handleMount}
+		/>
+	);
+}
+
+export default memo(MonacoEditor);
