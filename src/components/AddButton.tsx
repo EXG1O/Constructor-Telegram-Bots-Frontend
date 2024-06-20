60b750f8e5ed0f97bdeb651640113a508868b2ed
EXG1O <borodenko22448811@gmail.com>
2024-06-20 04:03:03 +0300

    Update AddButton component

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,11 +1,13 @@
-import React, { ReactElement, memo } from 'react';
+import React, { ReactElement, memo, useMemo } from 'react';
 import classNames from 'classnames';
+
+import PlusIcon from 'assets/icons/plus.svg';
 
 import Button, { ButtonProps } from 'react-bootstrap/Button';
 
 export type AddButtonProps = ButtonProps;
 
-const iconFontSizes: Record<NonNullable<AddButtonProps['size']>, number> = {
+const iconSizes: Record<NonNullable<AddButtonProps['size']>, number> = {
 	sm: 21,
 	lg: 30,
 };
@@ -16,22 +18,19 @@ function AddButton({
 	children,
 	...props
 }: AddButtonProps): ReactElement<AddButtonProps> {
-	const paddingStart: number = size === 'sm' ? 1 : 2;
-	const iconFontSize: number = size ? iconFontSizes[size] : 24;
+	const paddingStart = useMemo<number>(() => size === 'sm' ? 1 : 2, [size]);
+	const iconSize = useMemo<number>(() => size ? iconSizes[size] : 23, [size]);
 
 	return (
 		<Button
 			{...props}
 			size={size}
 			className={classNames(
-				`d-flex justify-content-center ps-${paddingStart}`,
+				`d-flex justify-content-center align-items-center ps-${paddingStart}`,
 				className,
 			)}
 		>
-			<i
-				className='d-flex bi bi-plus'
-				style={{ fontSize: `${iconFontSize}px` }}
-			/>
+			<PlusIcon width={iconSize} height={iconSize} />
 			{children}
 		</Button>
 	);
