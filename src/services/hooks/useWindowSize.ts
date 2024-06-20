997b9578d6072afb2dd76a518dbc0e9fe3f93675
EXG1O <borodenko22448811@gmail.com>
2024-06-20 03:44:32 +0300

    Create useWindowSize global hook

---
+++ src/services/hooks/useWindowSize.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+export interface WindowSize {
+	width: number;
+	height: number;
+}
+
+function useWindowSize(): WindowSize {
+	const [windowSize, setWindowSize] = useState<WindowSize>({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
+
+	useEffect(() => {
+		const handleResize = () =>
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+
+		window.addEventListener('resize', handleResize);
+
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return windowSize;
+}
+
+export default useWindowSize;
