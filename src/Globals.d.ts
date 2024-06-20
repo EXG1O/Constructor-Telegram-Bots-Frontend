6ab885dbdd043cff3e11f0bbe81fad9ead855639
EXG1O <borodenko22448811@gmail.com>
2024-06-20 03:32:25 +0300

    Update webpack config for future assets usage

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,5 +1,22 @@
 declare module '*.css';
 declare module '*.scss';
+declare module '*.png' {
+	export const URL: string;
+	export default URL;
+}
+declare module '*.jpg' {
+	export const URL: string;
+	export default URL;
+}
+declare module '*.svg?url' {
+	export const URL: string;
+	export default URL;
+}
+declare module '*.svg' {
+	import React from 'react';
+	export const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+	export default SVG;
+}
 
 declare function pluralidx(n: number): number;
 declare function gettext(msgid: string): string;
--- webpack.config.ts
+++ webpack.config.ts
@@ -10,68 +10,110 @@ interface Configuration extends WebpackConfiguration {
 	devServer?: WebpackDevServerConfiguration;
 }
 
-const config = (env: any, argv: any): Configuration => ({
-	entry: './src/index.tsx',
-	output: {
-		clean: true,
-		path: `${__dirname}/dist/frontend`,
-		filename: '[name].[contenthash].bundle.js',
-		chunkFilename: '[name].[contenthash].chunk.js',
-	},
-	devServer: env.WEBPACK_SERVE ? {
-		historyApiFallback: true,
-		proxy: {
-			context: ['/api/', '/media/'],
-			target: 'http://localhost:8000/',
+const config = (env: any, argv: any): Configuration => {
+	const isProduction: boolean = argv.mode !== 'production';
+
+	return {
+		entry: './src/index.tsx',
+		output: {
+			clean: true,
+			path: `${__dirname}/dist/frontend`,
+			filename: '[name].[contenthash].bundle.js',
+			chunkFilename: '[name].[contenthash].chunk.js',
 		},
-		static: `${__dirname}/dist/frontend`,
-		hot: true,
-	} : undefined,
-	module: {
-		rules: [
-			{
-				test: /\.(tsx?|jsx?)$/,
-				exclude: /node_modules/,
-				use: 'babel-loader',
-			},
-			{
-				test: /\.s?css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
-		alias: {
-			styles: `${__dirname}/src/styles/`,
-			components: `${__dirname}/src/components/`,
-			routes: `${__dirname}/src/routes/`,
-			services: `${__dirname}/src/services/`,
-			utils: `${__dirname}/src/utils/`,
+		devServer: env.WEBPACK_SERVE
+			? {
+					historyApiFallback: true,
+					proxy: {
+						context: ['/api/', '/media/'],
+						target: 'http://localhost:8000/',
+					},
+					static: `${__dirname}/dist/frontend`,
+					hot: true,
+				}
+			: undefined,
+		module: {
+			rules: [
+				{
+					test: /\.[jt]sx?$/,
+					exclude: /node_modules/,
+					use: 'babel-loader',
+				},
+				{
+					test: /\.s?css$/,
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				},
+				{
+					test: /\.(png|jpg)$/,
+					type: 'asset/resource',
+				},
+				{
+					test: /\.svg$/,
+					resourceQuery: /url/,
+					type: 'asset',
+				},
+				{
+					test: /\.svg$/,
+					issuer: /\.[jt]sx?$/,
+					resourceQuery: { not: [/url/] },
+					use: [
+						'babel-loader',
+						{
+							loader: '@svgr/webpack',
+							options: {
+								babel: false,
+								namedExport: 'SVG',
+								svgoConfig: {
+									plugins: [
+										{
+											name: 'preset-default',
+											params: {
+												overrides: {
+													removeViewBox: false,
+												},
+											},
+										},
+									],
+								},
+							},
+						},
+					],
+				},
+			],
 		},
-	},
-	optimization: {
-		minimizer: [
-			'...',
-			new CssMinimizerPlugin(),
+		resolve: {
+			extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+			alias: {
+				assets: `${__dirname}/src/assets/`,
+				styles: `${__dirname}/src/styles/`,
+				components: `${__dirname}/src/components/`,
+				routes: `${__dirname}/src/routes/`,
+				services: `${__dirname}/src/services/`,
+				utils: `${__dirname}/src/utils/`,
+			},
+		},
+		optimization: {
+			minimizer: ['...', new CssMinimizerPlugin()],
+		},
+		plugins: [
+			new Dotenv(),
+			new ForkTsCheckerWebpackPlugin(),
+			new HtmlWebpackPlugin(
+				isProduction
+					? {
+							template: './src/dev.html',
+							publicPath: '/',
+						}
+					: {
+							template: './src/prod.html',
+							publicPath: '/static/frontend/',
+						},
+			),
+			new MiniCssExtractPlugin({
+				filename: '[name].[contenthash].css',
+			}),
 		],
-	},
-	plugins: [
-		new Dotenv(),
-		new ForkTsCheckerWebpackPlugin(),
-		new HtmlWebpackPlugin(
-			argv.mode === 'development' ? {
-				template: './src/dev.html',
-				publicPath: '/',
-			} : {
-				template: './src/prod.html',
-				publicPath: '/static/frontend/',
-			}
-		),
-		new MiniCssExtractPlugin({
-			filename: '[name].[contenthash].css',
-		}),
-	],
-});
+	};
+};
 
 export default config;
