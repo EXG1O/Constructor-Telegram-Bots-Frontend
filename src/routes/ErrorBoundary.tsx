36e3aaf35f2b59a74bf054154d2676c2be395363
EXG1O <borodenko22448811@gmail.com>
2024-06-20 04:26:29 +0300

    Update ErrorBoundary route

--- src/routes/ErrorBoundary.tsx
+++ src/routes/ErrorBoundary.tsx
@@ -1,29 +1,22 @@
 import React, { ReactElement } from 'react';
 
-import Container from 'react-bootstrap/Container';
-
-import Title from 'components/Title';
+import Page from 'components/Page';
 
 const title: string = gettext('Ошибка');
 
 function ErrorBoundary(): ReactElement {
 	return (
-		<Title title={title}>
-			<main className='my-auto'>
-				<Container className='text-center my-3 my-lg-4'>
-					<h1 className='fw-semibold mb-1'>{title}</h1>
-					<p className='lead mb-0'>
-						{gettext(
-							'Проверьте ваше интернет-подключение или попробуйте перезагрузить страницу.',
-						)}{' '}
-						<br />
-						{gettext(
-							'Если ничего не помогло, свяжитесь с основателем проекта.',
-						)}
-					</p>
-				</Container>
-			</main>
-		</Title>
+		<Page title={title} align='center' className='text-center'>
+			<h1 className='fw-bold'>{title}</h1>
+			<p className='fs-5'>
+				{gettext(
+					'Проверьте ваше интернет-подключение или ' +
+						'попробуйте перезагрузить страницу.',
+				)}
+				<br />
+				{gettext('Если ничего не помогло, свяжитесь с основателем проекта.')}
+			</p>
+		</Page>
 	);
 }
 
