47a13e1831631ff7762b3423047097bcf540ce2d
exg1o <borodenko22448811@gmail.com>
2024-10-21 21:54:46 +0300

    Update global settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,10 @@
 const DEBUG: boolean = process.env.DEBUG === 'true';
 const WEBPACK_SERVE: boolean = process.env.WEBPACK_SERVE === 'true';
 const ENABLE_TELEGRAM_AUTH: boolean = process.env.ENABLE_TELEGRAM_AUTH === 'true';
-const PUBLIC_PATH: string = process.env.PUBLIC_PATH!;
+const PUBLIC_PATH: string | undefined = process.env.PUBLIC_PATH;
+
+if (PUBLIC_PATH === undefined) {
+	throw new Error('Please define the PUBLIC_PATH environment variable.');
+}
 
 export default { DEBUG, WEBPACK_SERVE, ENABLE_TELEGRAM_AUTH, PUBLIC_PATH };
