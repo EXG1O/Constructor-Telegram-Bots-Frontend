ab2482357f786e767d6e890b76c902c0d0c7fc99
exg1o <borodenko22448811@gmail.com>
2025-01-09 16:17:25 +0200

    Create `FormQuillEditorFeedback` component

---
+++ src/components/FormQuillEditorFeedback.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement, useCallback } from 'react';
+import { useField, useFormikContext } from 'formik';
+
+import QuillEditorFeedback, { QuillEditorFeedbackProps } from './QuillEditorFeedback';
+
+export interface FormQuillEditorFeedbackProps extends QuillEditorFeedbackProps {
+	name: string;
+}
+
+function FormQuillEditorFeedback({
+	name,
+	...props
+}: FormQuillEditorFeedbackProps): ReactElement<FormQuillEditorFeedbackProps> {
+	const { setFieldValue } = useFormikContext();
+	const [{ value, onBlur }, meta] = useField({ name });
+
+	const handleChange = useCallback<NonNullable<QuillEditorFeedbackProps['onChange']>>(
+		(value) => setFieldValue(name, value),
+		[name],
+	);
+
+	return (
+		<QuillEditorFeedback
+			{...props}
+			value={value}
+			error={meta.error}
+			onBlur={onBlur}
+			onChange={handleChange}
+		/>
+	);
+}
+
+export default FormQuillEditorFeedback;
