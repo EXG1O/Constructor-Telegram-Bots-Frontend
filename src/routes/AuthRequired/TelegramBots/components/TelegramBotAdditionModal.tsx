32e99bd7ffa9b92304149703cd500faf68839876
exg1o <borodenko22448811@gmail.com>
2025-01-10 11:16:48 +0200

    Update `TelegramBotAdditionModal` component

--- src/routes/AuthRequired/TelegramBots/components/TelegramBotAdditionModal.tsx
+++ src/routes/AuthRequired/TelegramBots/components/TelegramBotAdditionModal.tsx
@@ -1,36 +1,33 @@
-import React, { memo, ReactElement, useMemo, useState } from 'react';
+import React, { memo, ReactElement, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Form, Formik, FormikHelpers } from 'formik';
 
 import { RouteID } from 'routes';
 
-import Feedback from 'react-bootstrap/Feedback';
-import Input from 'react-bootstrap/FormControl';
-import Switch from 'react-bootstrap/Switch';
-
 import Button from 'components/Button';
+import FormCheckFeedback from 'components/FormCheckFeedback';
+import FormInputFeedback from 'components/FormInputFeedback';
 import Modal, { ModalProps } from 'components/Modal';
 import Stack from 'components/Stack';
 import { createMessageToast } from 'components/ToastContainer';
 
 import useTelegramBots from '../hooks/useTelegramBots';
 
-import { APIResponse } from 'services/api/core';
 import { TelegramBotsAPI } from 'services/api/telegram_bots/main';
 import { Data } from 'services/api/telegram_bots/types';
 
-type Data = Data.TelegramBotsAPI.Create;
+type FormValues = Data.TelegramBotsAPI.Create;
 
 export interface TelegramBotAdditionModalProps
-	extends Omit<ModalProps, 'loading' | 'children'> {
+	extends Omit<ModalProps, 'loading' | 'children' | 'onExited'> {
 	show: NonNullable<ModalProps['show']>;
 	onHide: NonNullable<ModalProps['onHide']>;
 }
 
-const defaultData: Data = { api_token: '', is_private: false };
+const defaultFormValues: FormValues = { api_token: '', is_private: false };
 
 function TelegramBotAdditionModal({
 	onHide,
-	onExited,
 	...props
 }: TelegramBotAdditionModalProps): ReactElement<TelegramBotAdditionModalProps> {
 	const { t } = useTranslation(RouteID.TelegramBots, {
@@ -39,87 +36,75 @@ function TelegramBotAdditionModal({
 
 	const [telegramBots, setTelegramBots] = useTelegramBots();
 
-	const [data, setData] = useState<Data>(defaultData);
-	const [errors, setErrors] = useState<APIResponse.ErrorDetail[]>([]);
-	const [loading, setLoading] = useState<boolean>(false);
+	async function handleSubmit(
+		values: FormValues,
+		{ setErrors }: FormikHelpers<FormValues>,
+	): Promise<void> {
+		const response = await TelegramBotsAPI.create(values);
 
-	const apiTokenError = useMemo<APIResponse.ErrorDetail | undefined>(
-		() => errors.find((error) => error.attr === 'api_token'),
-		[errors],
-	);
-	const privateError = useMemo<APIResponse.ErrorDetail | undefined>(
-		() => errors.find((error) => error.attr === 'is_private'),
-		[errors],
-	);
-
-	function handleExited(node: HTMLElement): void {
-		onExited?.(node);
-		setData(defaultData);
-	}
-
-	async function handleAdd(): Promise<void> {
-		setLoading(true);
-
-		const response = await TelegramBotsAPI.create(data);
-
-		if (response.ok) {
-			setTelegramBots([...telegramBots, response.json]);
-			onHide();
-			createMessageToast({
-				message: t('messages.createTelegramBot.success'),
-				level: 'success',
-			});
-		} else if (response.json.type === 'validation_error') {
-			setErrors(response.json.errors);
-		} else {
+		if (!response.ok) {
+			setErrors(
+				response.json.errors.reduce<Record<string, string>>(
+					(errors, { attr, detail }) => {
+						if (attr) errors[attr] = detail;
+						return errors;
+					},
+					{},
+				),
+			);
 			createMessageToast({
 				message: t('messages.createTelegramBot.error'),
 				level: 'error',
 			});
+			return;
 		}
 
-		setLoading(false);
+		setTelegramBots([...telegramBots, response.json]);
+		onHide();
+		createMessageToast({
+			message: t('messages.createTelegramBot.success'),
+			level: 'success',
+		});
 	}
 
 	return (
-		<Modal {...props} loading={loading} onHide={onHide} onExited={handleExited}>
-			<Modal.Header closeButton>
-				<Modal.Title>{t('title')}</Modal.Title>
-			</Modal.Header>
-			<Modal.Body as={Stack} gap={2}>
-				<div>
-					<Input
-						value={data.api_token}
-						isInvalid={Boolean(apiTokenError)}
-						placeholder={t('apiTokenInputPlaceholder')}
-						onChange={(e) =>
-							setData({ ...data, api_token: e.target.value })
-						}
-					/>
-					{apiTokenError && (
-						<Feedback type='invalid'>{apiTokenError.detail}</Feedback>
-					)}
-				</div>
-				<div>
-					<Switch
-						checked={data.is_private}
-						isInvalid={Boolean(privateError)}
-						label={t('privateSwitchLabel')}
-						onChange={(e) =>
-							setData({ ...data, is_private: e.target.checked })
-						}
-					/>
-					{privateError && (
-						<Feedback type='invalid'>{privateError.detail}</Feedback>
-					)}
-				</div>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button variant='success' onClick={handleAdd}>
-					{t('addButton')}
-				</Button>
-			</Modal.Footer>
-		</Modal>
+		<Formik
+			initialValues={defaultFormValues}
+			validateOnBlur={false}
+			validateOnChange={false}
+			onSubmit={handleSubmit}
+		>
+			{({ isSubmitting, resetForm }) => (
+				<Modal
+					{...props}
+					loading={isSubmitting}
+					onHide={onHide}
+					onExited={useCallback(() => resetForm(), [])}
+				>
+					<Form>
+						<Modal.Header closeButton>
+							<Modal.Title>{t('title')}</Modal.Title>
+						</Modal.Header>
+						<Modal.Body as={Stack} gap={2}>
+							<FormInputFeedback
+								name='api_token'
+								placeholder={t('apiTokenInputPlaceholder')}
+							/>
+							<FormCheckFeedback
+								type='switch'
+								name='is_private'
+								label={t('privateSwitchLabel')}
+							/>
+						</Modal.Body>
+						<Modal.Footer>
+							<Button variant='success' type='submit'>
+								{t('addButton')}
+							</Button>
+						</Modal.Footer>
+					</Form>
+				</Modal>
+			)}
+		</Formik>
 	);
 }
 
