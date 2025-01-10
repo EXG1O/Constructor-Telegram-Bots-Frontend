bd3c5efaf24a2bbab19e10019c2d536281fee9bf
exg1o <borodenko22448811@gmail.com>
2025-01-10 11:08:12 +0200

    Create `CheckFeedback` component

---
+++ src/components/CheckFeedback.tsx
@@ -0,0 +1,30 @@
+import React, { forwardRef } from 'react';
+
+import Check, { CheckProps } from './Check';
+
+import { FCA } from 'utils/helpers';
+
+import('./CheckFeedback.scss');
+import('./Feedback.scss');
+
+export interface CheckFeedbackProps extends Omit<CheckProps, 'isInvalid'> {
+	error?: string;
+}
+
+const CheckFeedback: FCA<'input', CheckFeedbackProps> = forwardRef<
+	HTMLElement,
+	CheckFeedbackProps
+>(function CheckFeedback({ as, error, ...props }, ref) {
+	return (
+		<Check
+			as={as}
+			ref={ref}
+			{...props}
+			isInvalid={Boolean(error)}
+			feedbackType='invalid'
+			feedback={error}
+		/>
+	);
+});
+
+export default CheckFeedback;
