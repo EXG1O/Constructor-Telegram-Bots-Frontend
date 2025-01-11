6f7b5de11040b57dd6a8d41087929e3bf70224bd
exg1o <borodenko22448811@gmail.com>
2025-01-11 10:24:09 +0200

    Update `NameBlock` component

---
+++ src/routes/AuthRequired/TelegramBotMenu/Constructor/components/NameBlock.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { RouteID } from 'routes';
+
+import FormInputFeedback from 'components/FormInputFeedback';
+
+import Block, { BlockProps } from './Block';
+
+export type Name = string;
+
+export type NameBlockProps = Pick<BlockProps, 'className'>;
+
+export const defaultName: Name = '';
+
+function NameBlock(props: NameBlockProps): ReactElement<NameBlockProps> {
+	const { t } = useTranslation(RouteID.TelegramBotMenuConstructor, {
+		keyPrefix: 'nameBlock',
+	});
+
+	return (
+		<Block {...props} title={t('title')} body>
+			<FormInputFeedback name='name' placeholder={t('inputPlaceholder')} />
+		</Block>
+	);
+}
+
+export default NameBlock;
