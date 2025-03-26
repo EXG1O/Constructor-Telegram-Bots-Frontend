d4b8f82379ebc44d13ce9ef10066570585a5090b
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-03-26 22:20:28 +0200

    Migrate dynamic content from `HTML` to `Markdown` from `API` (#16)

--- src/routes/Donation/Index/components/SectionDisplay.tsx
+++ src/routes/Donation/Index/components/SectionDisplay.tsx
@@ -1,6 +1,8 @@
 import React, { HTMLAttributes, ReactElement } from 'react';
 import classNames from 'classnames';
 
+import Markdown from 'components/Markdown';
+
 import { Section } from 'api/donations/types';
 
 import('styles/dynamic-content.scss');
@@ -17,7 +19,7 @@ function SectionDisplay({
   return (
     <div {...props} className={classNames('dynamic-content', className)}>
       <h3 className='mb-1'>{section.title}</h3>
-      <div dangerouslySetInnerHTML={{ __html: section.text }} />
+      <Markdown>{section.text}</Markdown>
     </div>
   );
 }
