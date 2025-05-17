15f7e8c7551e2cd03811677f08aed18b4b0bc2a4
exg1o <exg1o.me@gmail.com>
2025-05-17 19:24:40 +0300

    Prepare everything for rewriting components to `tailwindcss`

---
+++ src/utils/cn.ts
@@ -0,0 +1,8 @@
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
+
+export default cn;
