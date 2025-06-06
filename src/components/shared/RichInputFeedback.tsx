a078fa58a6d7906fc67cd9380f1a46bd1943cd14
exg1o <exg1o.me@gmail.com>
2025-06-06 15:50:01 +0300

    Create `RichInputFeedback` component

---
+++ src/components/shared/RichInputFeedback.tsx
@@ -0,0 +1,26 @@
+import React, { HTMLAttributes, ReactElement } from 'react';
+
+import Feedback from 'components/ui/Feedback';
+import RichInput, { RichInputProps } from 'components/ui/RichInput';
+
+import cn from 'utils/cn';
+
+export interface RichInputFeedbackProps extends RichInputProps {
+  error?: string;
+  wrapperProps?: HTMLAttributes<HTMLDivElement>;
+}
+
+function RichInputFeedback({
+  error,
+  wrapperProps,
+  ...props
+}: RichInputFeedbackProps): ReactElement {
+  return (
+    <div {...wrapperProps} className={cn('w-full', wrapperProps?.className)}>
+      <RichInput {...props} invalid={Boolean(error)} />
+      {error && <Feedback type='invalid'>{error}</Feedback>}
+    </div>
+  );
+}
+
+export default RichInputFeedback;
