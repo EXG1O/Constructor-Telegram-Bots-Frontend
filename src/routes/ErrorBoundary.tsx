7cc07512144b2657d3532c44d0aad2bb7516414b
exg1o <exg1o.me@gmail.com>
2025-06-13 19:43:28 +0300

    Migrate `ErrorBoundary` route

--- src/routes/ErrorBoundary.tsx
+++ src/routes/ErrorBoundary.tsx
@@ -1,17 +1,17 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
 import Page from 'components/shared/Page';
 
 function ErrorBoundary(): ReactElement {
-  const { t, i18n } = useTranslation('error-boundary');
+  const { t } = useTranslation('error-boundary');
 
-  const title = useMemo<string>(() => t('title'), [i18n.language]);
+  const title: string = t('title');
 
   return (
-    <Page title={title} align='center' className='text-center'>
-      <h1 className='fw-bold'>{title}</h1>
-      <p className='fs-5'>
+    <Page title={title} align='center' className='text-center text-foreground'>
+      <h1 className='text-5xl font-bold'>{title}</h1>
+      <p className='text-xl'>
         <Trans t={t} i18nKey='text' components={[<br key={0} />]} />
       </p>
     </Page>
