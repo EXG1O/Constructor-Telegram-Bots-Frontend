5e532ca67d097619bc2db875e0702f15a4434251
exg1o <exg1o.me@gmail.com>
2025-06-13 21:13:09 +0300

    Migrate `Donation/Completed` route

--- src/routes/Donation/Completed.tsx
+++ src/routes/Donation/Completed.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useMemo } from 'react';
+import React, { ReactElement } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
 import { RouteID } from 'routes';
@@ -6,14 +6,14 @@ import { RouteID } from 'routes';
 import Page from 'components/shared/Page';
 
 function Completed(): ReactElement {
-  const { t, i18n } = useTranslation(RouteID.DonationCompleted);
+  const { t } = useTranslation(RouteID.DonationCompleted);
 
-  const title = useMemo<string>(() => t('title'), [i18n.language]);
+  const title: string = t('title');
 
   return (
-    <Page title={title} align='center' grid className='text-center'>
-      <h1 className='fw-semibold'>{title}</h1>
-      <p className='lead'>
+    <Page title={title} align='center' className='text-center text-foreground'>
+      <h2 className='text-4xl font-semibold'>{title}</h2>
+      <p className='text-lg'>
         <Trans t={t} i18nKey='text' components={[<br key={0} />]} />
       </p>
     </Page>
