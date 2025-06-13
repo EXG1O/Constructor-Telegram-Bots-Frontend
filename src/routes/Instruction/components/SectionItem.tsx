9f6e1b3853e580befa0ca880d9642f200da6ea43
exg1o <exg1o.me@gmail.com>
2025-06-13 20:58:13 +0300

    Migrate `Instruction` route

---
+++ src/routes/Instruction/components/SectionItem.tsx
@@ -0,0 +1,23 @@
+import React, { HTMLAttributes, ReactElement } from 'react';
+
+import Markdown from 'components/ui/Markdown';
+
+import { Section } from 'api/instruction/types';
+
+import cn from 'utils/cn';
+
+export interface SectionItemProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
+  section: Section;
+}
+
+function SectionItem({ section, className, ...props }: SectionItemProps): ReactElement {
+  return (
+    <div {...props} className={cn('text-foreground', className)}>
+      <h3 className='mb-1 text-3xl font-semibold'>{section.title}</h3>
+      <Markdown>{section.text}</Markdown>
+    </div>
+  );
+}
+
+export default SectionItem;
