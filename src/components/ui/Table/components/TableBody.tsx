ab4f239d13e0adc799f2d4b1ef817df06bf90ad4
exg1o <exg1o.me@gmail.com>
2025-06-24 16:27:33 +0300

    Update `Table` component

--- src/components/ui/Table/components/TableBody.tsx
+++ src/components/ui/Table/components/TableBody.tsx
@@ -1,36 +1,10 @@
 import React, { forwardRef, HTMLAttributes } from 'react';
-import { cva } from 'class-variance-authority';
-
-import useTable from '../hooks/useTable';
-
-import cn from 'utils/cn';
-
-export const tableBodyVariants = cva([], {
-  variants: {
-    striped: {
-      true: ['[&_tr]:not-even:bg-foreground/5'],
-    },
-  },
-  defaultVariants: {
-    striped: false,
-  },
-});
 
 export interface TableBodyProps extends HTMLAttributes<HTMLTableSectionElement> {}
 
-const TableBody = forwardRef<HTMLTableSectionElement, TableBodyProps>(
-  ({ className, ...props }, ref) => {
-    const { striped } = useTable();
-
-    return (
-      <tbody
-        {...props}
-        ref={ref}
-        className={cn(tableBodyVariants({ striped, className }))}
-      />
-    );
-  },
-);
+const TableBody = forwardRef<HTMLTableSectionElement, TableBodyProps>((props, ref) => {
+  return <tbody {...props} ref={ref} />;
+});
 TableBody.displayName = 'TableBody';
 
 export default TableBody;
