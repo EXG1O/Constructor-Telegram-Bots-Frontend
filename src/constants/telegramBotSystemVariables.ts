bd7034b0cc268b74a50fbfacd4b83468420dad7f
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-01-12 15:22:41 +0200

    Add new text variables to `telegramBotSystemVariables` constant (#112)

--- src/constants/telegramBotSystemVariables.ts
+++ src/constants/telegramBotSystemVariables.ts
@@ -2,17 +2,28 @@ export type TelegramBotSystemVariablesType = 'personal' | 'global';
 
 const telegramBotSystemVariables: Record<TelegramBotSystemVariablesType, string[]> = {
   personal: [
+    'CHAT_ID',
+    'CHAT_TYPE',
+    'CHAT_NAME',
+    'CHAT_USERNAME',
+    'CHAT_FULL_NAME',
+    'CHAT_LINK',
     'USER_ID',
+    'USER_IS_BOT',
+    'USER_IS_PREMIUM',
+    'USER_NAME',
     'USER_USERNAME',
     'USER_FIRST_NAME',
     'USER_LAST_NAME',
     'USER_FULL_NAME',
     'USER_LANGUAGE_CODE',
+    'USER_LINK',
     'USER_MESSAGE_ID',
     'USER_MESSAGE_TEXT',
     'USER_MESSAGE_DATE',
+    'USER_MESSAGE_LINK',
   ],
-  global: ['BOT_NAME', 'BOT_USERNAME'],
+  global: ['BOT_ID', 'BOT_NAME', 'BOT_USERNAME', 'BOT_FULL_NAME', 'BOT_LINK'],
 };
 
 export default telegramBotSystemVariables;
