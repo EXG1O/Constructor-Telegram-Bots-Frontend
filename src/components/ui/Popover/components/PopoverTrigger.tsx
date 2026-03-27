bcec45f0c6b147dd7d0bdc739e8262f31eef70fa
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-03-27 16:33:11 +0200

    Enhance `Popover` component (#152)

--- src/components/ui/Popover/components/PopoverTrigger.tsx
+++
@@ -1,4 +0,0 @@
-import { PopoverTrigger, type PopoverTriggerProps } from '@radix-ui/react-popover';
-
-export type { PopoverTriggerProps };
-export default PopoverTrigger;
---
+++ src/components/ui/Popover/components/PopoverTrigger/components/TriggerQuestionIcon.tsx
@@ -0,0 +1,7 @@
+import { CircleQuestionMark, type LucideProps } from 'lucide-react';
+
+const TriggerQuestionIcon = CircleQuestionMark;
+TriggerQuestionIcon.displayName = 'TriggerQuestionIcon';
+
+export type { LucideProps as TriggerQuestionIconProps };
+export default TriggerQuestionIcon;
